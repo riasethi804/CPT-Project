cda329c7cff3a074ee4fc53c85777bdd0b0949bc
riasethi804 <150973387+riasethi804@users.noreply.github.com>
2023-12-01 13:42:02 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,26 +2,41 @@ document.addEventListener('DOMContentLoaded', function()
 {
   let startButton = document.getElementById('startButton');
   startButton.addEventListener('click', clicked);
+  // Function to handle the click event on the start button
 });
 
 function clicked() 
 {
   let selectedLevel = document.querySelector('input[name="level"]:checked');
+  // Get the selected character from the radio buttons
   let selectedCharacter = document.querySelector('input[name="character"]:checked');
   if (!selectedLevel || !selectedCharacter) 
   {
     alert("Please select both a difficulty level and a character before starting the game.");
-    return;  
+    return;
   }
- 
+  // Retrieve the value of the selected level
   let levelValue = selectedLevel.value;
-  if (levelValue == "Easy") 
-  {
-    window.location.href = "EasyPage.html";
-  }
+  let characterImageSrc = selectedCharacter.nextElementSibling.src;
   
-  else if (levelValue == "Hard") 
+  window.location.href = `${levelValue}Page.html?character=${encodeURIComponent(characterImageSrc)}`;
+}
+
+function appendCharacterImage()
+{
+ // Retrieve the character image source from the query parameters of the URL
+  let params = new URLSearchParams(window.location.search);
+  let characterImageSrc = params.get('character');
+  if (characterImageSrc) 
   {
-    window.location.href = "HardPage.html";
+    let characterImage = document.createElement('img');
+  // Set the image source to the one obtained from the URL query 
+    characterImage.src = characterImageSrc;
+    characterImage.alt = 'Selected Character';
+    let container = document.getElementById('right-container');
+  // Append the new image element to the container
+    container.appendChild(characterImage);
   }
 }
+document.addEventListener('DOMContentLoaded', appendCharacterImage);
+// Function to append the selected character image to the page
